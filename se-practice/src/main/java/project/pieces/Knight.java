124d9304c56f0beb3e8db6d9987d68ca4133816e
daniellipshitz <dlipshit@mail.yu.edu>
2023-05-03 16:55:44 -0400

    fixed rook

--- se-practice/src/main/java/project/pieces/Knight.java
+++ se-practice/src/main/java/project/pieces/Knight.java
@@ -1,6 +1,16 @@
-import ChessPiece;
+package project.pieces;
 
-package pieces;
+import project.ChessPiece;
+
 public class Knight extends ChessPiece {
-    
+    @Override
+    public boolean isLegalMove(int x1, int y1, int x2, int y2) {
+        int dx = Math.abs(x2 - x1);
+        int dy = Math.abs(y2 - y1);
+
+        if ((dx == 2 && dy == 1) || (dx == 1 && dy == 2)) {
+            return true;
+        }
+        return false;
+    }
 }
--- se-practice/src/main/java/project/pieces/Rook.java
+++ se-practice/src/main/java/project/pieces/Rook.java
@@ -1,16 +1,6 @@
-package project.pieces;
+import ChessPiece;
 
-import project.ChessPiece;
-
+package pieces;
 public class Rook extends ChessPiece {
-    @Override
-    public boolean isLegalMove(int x1, int y1, int x2, int y2) {
-        int dx = Math.abs(x2 - x1);
-        int dy = Math.abs(y2 - y1);
-
-        if ((dx == 2 && dy == 1) || (dx == 1 && dy == 2)) {
-            return true;
-        }
-        return false;
-    }
+    
 }
