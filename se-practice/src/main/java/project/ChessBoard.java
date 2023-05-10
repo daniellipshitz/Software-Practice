0e88bcc27c3f10f8b8ba6866d4b502a155206791
daniellipshitz <dlipshit@mail.yu.edu>
2023-05-10 13:27:55 -0400

    daniel 1:27 pm

--- se-practice/src/main/java/project/ChessBoard.java
+++ se-practice/src/main/java/project/ChessBoard.java
@@ -23,26 +23,26 @@ public class ChessBoard{
 
     private void initializeBoard() {
         board[0][0] = new Rook(true, new int[]{0, 0});
-        board[1][0] = new Knight(true, new int[]{0, 1});
-        board[2][0] = new Bishop(true, new int[]{0, 2});
-        board[3][0] = new Queen(true, new int[]{0, 3});
-        board[4][0] = new King(true, new int[]{0, 4});
-        board[5][0] = new Bishop(true, new int[]{0, 5});
-        board[6][0] = new Knight(true, new int[]{0, 6});
-        board[7][0] = new Rook(true, new int[]{0, 7});
+        board[1][0] = new Knight(true, new int[]{1, 0});
+        board[2][0] = new Bishop(true, new int[]{2, 0});
+        board[3][0] = new Queen(true, new int[]{3, 0});
+        board[4][0] = new King(true, new int[]{4, 0});
+        board[5][0] = new Bishop(true, new int[]{5, 0});
+        board[6][0] = new Knight(true, new int[]{6, 0});
+        board[7][0] = new Rook(true, new int[]{7, 0});
 
         for (int i = 0; i < 8; i++) {
-            board[i][1] = new Pawn(true, new int[]{1, i});
-            board[i][6] = new Pawn(false, new int[]{6, i});
+            board[i][1] = new Pawn(true, new int[]{i, 1});
+            board[i][6] = new Pawn(false, new int[]{i, 6});
         }
         
-        board[0][7] = new Rook(false, new int[]{7, 0});
-        board[1][7] = new Knight(false, new int[]{7, 1});
-        board[2][7] = new Bishop(false, new int[]{7, 2});
-        board[3][7] = new Queen(false, new int[]{7, 3});
-        board[4][7] = new King(false, new int[]{7, 4});
-        board[5][7] = new Bishop(false, new int[]{7, 5});
-        board[6][7] = new Knight(false, new int[]{7, 6});
+        board[0][7] = new Rook(false, new int[]{0, 7});
+        board[1][7] = new Knight(false, new int[]{1, 7});
+        board[2][7] = new Bishop(false, new int[]{2, 7});
+        board[3][7] = new Queen(false, new int[]{3, 7});
+        board[4][7] = new King(false, new int[]{4, 7});
+        board[5][7] = new Bishop(false, new int[]{5, 7});
+        board[6][7] = new Knight(false, new int[]{6, 7});
         board[7][7] = new Rook(false, new int[]{7, 7});
 
         moveTree.addMove(new Move());
