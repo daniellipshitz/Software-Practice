ecd76b6e2fc68292cd14862db42450e6f0af61cc
daniellipshitz <dlipshit@mail.yu.edu>
2023-05-09 20:40:31 -0400

    Daniel Tuesday 8:40 pm

--- se-practice/src/main/java/project/pieces/Rook.java
+++ se-practice/src/main/java/project/pieces/Rook.java
@@ -15,7 +15,7 @@ public class Rook extends ChessPiece {
         int dx = Math.abs(x2 - x1);
         int dy = Math.abs(y2 - y1);
 
-        if ((dx == 2 && dy == 1) || (dx == 1 && dy == 2)) {
+        if (dx == dy) {
             return true;
         }
         return false;
